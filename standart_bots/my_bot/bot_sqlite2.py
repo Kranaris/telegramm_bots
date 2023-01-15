caf6dda4907ffa5e3dd16a04eff2663b37167411
Nikita Karavaev <nikitakaravaev@mail.ru>
2023-01-15 23:25:00 +0300

    products_v_1

--- standart_bots/my_bot/bot_sqlite2.py
+++ standart_bots/my_bot/bot_sqlite2.py
@@ -15,34 +15,49 @@ storage = MemoryStorage()
 bot = Bot(API_TOKEN)
 dp = Dispatcher(bot, storage=storage)
 
+
 async def on_startup(_):
     await sqlite.db_connect()
     print("Data base connected successfully")
     print("The bot has been started successfully!")
 
-class ProfilestatesGroup(StatesGroup):
-    name = State()
-    name = State()
+
+class Product_statesGroup(StatesGroup):
+    title = State()
     description = State()
+
 
 class CustomMiddleware(BaseMiddleware):
     async def on_process_message(self,
-                                message: types.Message,
-                                data: dict):
+                                 message: types.Message,
+                                 data: dict):
         if message.from_user.id != ADMIN:
             print(message.from_user.id)
             raise CancelHandler()
 
+
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message) -> None:
-    await message.answer(f'Welcome, {message.from_user.first_name}!',reply_markup=get_start_kb())
+    await message.answer(f'Welcome, {message.from_user.first_name}!',
+                         reply_markup=get_start_kb())
     await message.delete()
+
 
 @dp.message_handler(commands=['products_management'])
 async def products_management_command(message: types.Message) -> None:
     await message.answer(f'Products_management menu.',
                          reply_markup=get_start_ikb())
     await message.delete()
+
+
+@dp.message_handler(commands=['cancel'], state="*")
+async def cancel_command(message: types.Message, state: FSMContext) -> None:
+    if state is None:
+        return
+    await message.reply('Add was canceled!', reply_markup=get_start_kb())
+
+    await state.finish()
+
 
 @dp.callback_query_handler(text='get_all_products')
 async def cb_get_all_products(callback: types.CallbackQuery):
@@ -52,12 +67,45 @@ async def cb_get_all_products(callback: types.CallbackQuery):
         await callback.answer("You don't have any products!")
         return await callback.answer()
 
+    await callback.message.answer(products)
+    await callback.answer()
+
+
+@dp.callback_query_handler(text='add_new_product')
+async def cb_add_new_product(callback: types.CallbackQuery):
+    await callback.message.delete()
+    await callback.message.answer("Text product's title:",
+                                  reply_markup=get_cancel())
+
+    await Product_statesGroup.title.set()
+
+
+@dp.message_handler(state=Product_statesGroup.title)
+async def handle_title(message: types.Message, state: FSMContext) -> None:
+    async with state.proxy() as data:
+        data['title'] = message.text
+
+    await message.reply("Text product's description:")
+    await Product_statesGroup.next()
+
+
+@dp.message_handler(state=Product_statesGroup.description)
+async def handle_title(message: types.Message, state: FSMContext) -> None:
+    async with state.proxy() as data:
+        data['description'] = message.text
+    await sqlite.create_newproduct(state)
+    await message.reply("The product has been added!",
+                        reply_markup=get_start_ikb())
+
+    await state.finish()
+
+
 @dp.message_handler(commands=['cancel'], state="*")
-async def cmd_start(message: types.Message, state: FSMContext) -> None:
-    current_state = await state.get_state()
-    if current_state is None:
+async def cancel_command(message: types.Message, state: FSMContext) -> None:
+    if state is None:
         return
-    await message.reply('Add was canceled!', reply_markup=get_start_ikb())
+    await message.reply('Add was canceled!', reply_markup=get_start_kb())
+
     await state.finish()
 
 
--- standart_bots/my_bot/sqlite.py
+++ standart_bots/my_bot/sqlite.py
@@ -8,18 +8,19 @@ async def db_connect():
     cur = db.cursor()
 
     cur.execute(
-        "CREATE TABLE IF NOT EXISTS my_products(product_id INTEGER PRIMARY KEY, name TEXT, price, description TEXT)")
+        "CREATE TABLE IF NOT EXISTS my_products(title TEXT, description TEXT)")
 
     db.commit()
 
-async def get_all_products_bd():
 
+async def get_all_products_bd():
     products = cur.execute("SELECT * FROM my_products").fetchall()
 
     return products
-async def add_profile(state, user_id):
+
+
+async def create_newproduct(state):
     async with state.proxy() as data:
-        cur.execute(
-            "UPDATE profiles SET photo = '{}', age = '{}', description = '{}', name = '{}' WHERE user_id == '{}'".format(
-                data['photo'], data['age'], data['description'], data['name'], user_id))
+        new_product = cur.execute("INSERT INTO my_products VALUES (?, ?)", (data['title'], data['description']))
         db.commit()
+    return new_product
